a16275bedc541752ef413a2d7a8a9b38e2b57f5a
Buzzer Felix <101106797+BuzzerFelix@users.noreply.github.com>
2022-04-21 11:38:27 +0300

    Add files via upload

---
+++ Source Code/RustClientPIDFinder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RustClientPIDFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] proc = Process.GetProcessesByName("RustClient");
+            if (proc.Length == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("RustClient is Detected");
+                Thread.Sleep(2000);
+                Console.Clear();
+            }
+            else
+            {
+                MessageBox.Show("RustClient not Found, Please Run RustClient(Steam or Pirated Version) and Try Again");
+                Environment.Exit(431);
+            }
+            foreach (var rust in proc)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"RustClient's PID: {rust.Id}");
+                long kl = rust.PagedSystemMemorySize64; //EAC is Defending Memory, and it's not showing :/
+                Console.Write(kl.ToString());
+                long xxx = rust.PrivateMemorySize64; //WTF EAC, why? :( 
+                Console.Write(xxx.ToString());
+            }
+            Thread.Sleep(5000);
+            Environment.Exit(0);
+        }
+    }
+}
